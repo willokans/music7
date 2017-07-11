5c6cbcae625f383e02b12f17681b0d2ffe1e40c4
will.okanumeh <will.okanumeh@ellucian.com>
2017-07-11 17:18:23 +0100

    menu action bar added but design not completed

--- app/src/main/java/musicshow/willokans/com/music7/HomeActivity.java
+++ app/src/main/java/musicshow/willokans/com/music7/HomeActivity.java
@@ -1,12 +1,14 @@
 package musicshow.willokans.com.music7;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -21,46 +23,58 @@ public class HomeActivity extends AppCompatActivity {
 
         hSignOut = (Button) findViewById(R.id.hSignOut);
 
-        hSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        //add icon to the action bar
+//        android.app.ActionBar actionBar = getActionBar();
+//        //call setlogo
+//        actionBar.setLogo(R.mipmap.ic_launcher);
+//        actionBar.setDisplayUseLogoEnabled(true);
+//        //add icon to the action bar
+//        actionBar.setDisplayShowHomeEnabled(true);
 
+
+
+    }
+
+
+    //for Action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //add items into the action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Toast.makeText(getApplicationContext(), "Search option selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_exit:
+                //Write own logic
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(HomeActivity.this, MainActivity.class));
-            }
-        });
+                return true;
+            case R.id.action_setting:
+                Toast.makeText(getApplicationContext(), "setting option selected", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
+
+
+    //            hSignOut.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
 //
-//    //for Action bar
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main, menu);
-//
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if(id==R.id.menu_profile) {
-//
-//            //Write own logic
-//
-//            return true;
-//        }
-//
-//        if (id==R.id.menu_setting) {
-//
-//            //write logic
-//
-//            return true;
-//        }
-//
-//        if (id==R.id.menu_logOut) {
-//
-//            return true;
-//        }
-//        return true;
-//    }
+//                    FirebaseAuth.getInstance().signOut();
+//                    startActivity(new Intent(HomeActivity.this, MainActivity.class));
+//                }
+//            });
+
 
 }
